41f28214788275cdf6c459f3a23f4c20e6d7f872
Gerard Compion <gcompion93@gmail.com>
2022-01-19 14:27:02 -0500

    added client.js and moved connect function to an object being exported from client. confirmed that everything still works

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+module.exports = {
+  connect: () => {
+    // establishes connection to server
+    const conn = net.createConnection({
+      host: '165.227.47.243',
+      port: 50541
+    });
+
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+
+    // message output after connecting successfully
+    conn.on('connect', () => {
+      console.log('Connection established!');
+    });
+
+    // so far the data in question is a rude message on death by idling...
+    conn.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    return conn;
+  }
+}
--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '99.248.219.172',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    console.log('Connection established!');
-  });
-
-  conn.on('data', (data) => {
-    console.log(data.toString());
-  })
-
-  return conn;
-};
+const clientObject = require("./client");
 
 console.log("Connecting ...");
-connect();
+clientObject.connect();
